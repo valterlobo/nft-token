8fa4912816479c0fc9212befd1de21965db07436
valterlobo <valterlobo@gmail.com>
2022-02-20 17:07:31 -0300

    start:erc 721 contract

---
+++ README.md
@@ -0,0 +1,14 @@
+https://dev.to/emanuelferreira/how-to-mint-an-nft-via-cli-25ea
+
+yarn es ./src/mintNft <privatekey> <tokenuri>
+
+npx hardhat run scripts/deploy.js --network rinkeby
+
+0xb8176F9BB9F6Ba77b0eB2449304E7009d7413d5d
+
+
+
+
+ yarn es src/mintNFT.ts xxxxxxxxxxxxxxx39853f3b66214b9a65dc3aeyyyyyyyyyyyyy https://web3dev-forem-production.s3.amazonaws.com/uploads/articles/rj0dq4ypo66dv2f7mn45.png
+
+
---
+++ contracts/factoryNFT.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract FactoryNFT is ERC721URIStorage { 
+
+    using Counters for Counters.Counter; 
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Factory NFT", "FTN") {
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+}
+
+
+
